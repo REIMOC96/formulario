a0c12682719e4c3b4a41f35969ecd33a91795135
REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-11-13 23:10:05 -0300

    angular 3 formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {HttpClientModule}  from '@angular/common/http'
+import {FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormularioComponent } from './formulario/formulario.component'; 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule, FormsModule,
+
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
