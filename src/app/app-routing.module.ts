87636f8a46c5f28f398fd89e5b62e2443043b93e
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2023-11-14 03:48:23 -0300

    renew 2 trabajo desde casa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './formulario/formulario.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
-  {path: "Formulario", component: FormularioComponent}
+
+  //url for FormularioComponent, like Formulario
+  {path: "Formulario", component: FormularioComponent},
+
+  //inicio url for InicioComponent, like "Inicio"
+  {path: "Inicio", component: InicioComponent}
+  
 ];
 
 @NgModule({
